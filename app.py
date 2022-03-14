46697bf50a98315d9f03927a50d6a1c9ff47cb3b
Ryan JT <ryantalbot7@gmail.com>
2022-03-13 19:22:19 -0500

    update example and app

--- app.py
+++ app.py
@@ -2,38 +2,44 @@ from flask import Flask, request, url_for, redirect, render_template
 import pandas as pd
 import pickle
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-model = pickle.load(open("example_weights_knn.pkl", "rb"))
+# model = pickle.load(open("example_weights_knn.pkl", "rb"))
+
+
+# @app.route('/')
+# def use_template():
+#     return render_template("index.html")
+
+
+# @app.route('/predict', methods=['POST', 'GET'])
+# def predict():
+#     input_one = request.form['1']
+#     input_two = request.form['2']
+#     input_three = request.form['3']
+#     input_four = request.form['4']
+#     input_five = request.form['5']
+#     input_six = request.form['6']
+#     input_seven = request.form['7']
+#     input_eight = request.form['8']
+
+#     setup_df = pd.DataFrame([pd.Series([input_one, input_two, input_three,
+#                             input_four, input_five, input_six, input_seven, input_eight])])
+#     diabetes_prediction = model.predict_proba(setup_df)
+#     output = '{0:.{1}f'.format(diabetes_prediction[0][1], 2)
+#     output = str(float(output)*100)+'%'
+#     if output > str(0.5):
+#         return render_template('result.html', pred=f'You have teh following chance of having diabetes based on our KNN model.\nProbability of having Diabetes is {output}')
+#     else:
+#         return render_template('result.html', pred=f'You have a low chance of diabetes which is currently considered safe (this is only an exmple)\n Probability of having diabetes is {output}')
+
+
+from flask import Flask, url_for, request, render_template
+
+app = Flask(__name__)
 
 
 @app.route('/')
-def use_template():
-    return render_template("index.html")
-
-
-@app.route('/predict', methods=['POST', 'GET'])
-def predict():
-    input_one = request.form['1']
-    input_two = request.form['2']
-    input_three = request.form['3']
-    input_four = request.form['4']
-    input_five = request.form['5']
-    input_six = request.form['6']
-    input_seven = request.form['7']
-    input_eight = request.form['8']
-
-    setup_df = pd.DataFrame([pd.Series([input_one, input_two, input_three,
-                            input_four, input_five, input_six, input_seven, input_eight])])
-    diabetes_prediction = model.predict_proba(setup_df)
-    output = '{0:.{1}f'.format(diabetes_prediction[0][1], 2)
-    output = str(float(output)*100)+'%'
-    if output > str(0.5):
-        return render_template('result.html', pred=f'You have teh following chance of having diabetes based on our KNN model.\nProbability of having Diabetes is {output}')
-    else:
-        return render_template('result.html', pred=f'You have a low chance of diabetes which is currently considered safe (this is only an exmple)\n Probability of having diabetes is {output}')
-
-
-if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=80)
-    app.run(debug=True)
+def index():
+    if __name__ == '__main__':
+        app.run(debug=True)
